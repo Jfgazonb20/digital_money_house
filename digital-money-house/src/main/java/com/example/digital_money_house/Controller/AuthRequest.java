cfbfa180025725dcc87164a23c5980daac1b43ab
Jfgazonb20 <jfgazonb@gmail.com>
2024-11-19 20:01:33 -0500

    Eliminacion carpeta de mas

--- digital-money-house/src/main/java/com/example/digital_money_house/Controller/AuthRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.digital_money_house.Controller;
-
-import lombok.Data;
-
-@Data
-public class AuthRequest {
-    private String username;
-    private String password;
-}
--- digital-money-house/src/main/java/com/example/digital_money_house/Exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.digital_money_house.Exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
